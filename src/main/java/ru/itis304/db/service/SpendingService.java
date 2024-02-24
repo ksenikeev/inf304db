d81f6f761c226172866e5333026ba8e524d721cf
Denys Patsera <nozistance@gmail.com>
2024-02-24 17:38:21 +0300

    Refactor SpendingService

--- src/main/java/ru/itis304/db/service/SpendingService.java
+++ src/main/java/ru/itis304/db/service/SpendingService.java
@@ -1,25 +1,16 @@
 package ru.itis304.db.service;
 
-import ru.itis304.db.model.Spending;
 import ru.itis304.db.model.SpendingData;
-import ru.itis304.db.repository.SpendingRepository;
+
 import java.util.List;
 
 public interface SpendingService {
 
-    public void addSpending(String date, Float sum, int spendingCode, String description) {
+    void save(SpendingData spendingData);
 
-    }
+    List<SpendingData> getAll();
 
-    public List<SpendingData> getAll() {
-        return null;
-    }
+    List<SpendingData> getAllByPeriod(String dateFrom, String dateTo);
 
-    public List<SpendingData> getAllByPeriod(String dateFrom, String dateTo) {
-        return null;
-    }
-
-    public Float getSumByPeriod(String dateFrom, String dateTo) {
-        return null;
-    }
+    Float getSumByPeriod(String dateFrom, String dateTo);
 }

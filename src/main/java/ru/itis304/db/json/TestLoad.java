38d73c3de6f65e570e445e1905c0fcedd52dca58
Nbveh25 <tima.bikmukhametov@inbox.ru>
2024-02-24 14:39:04 +0300

    Update TestLoad

--- src/main/java/ru/itis304/db/json/TestLoad.java
+++ src/main/java/ru/itis304/db/json/TestLoad.java
@@ -2,16 +2,11 @@ package ru.itis304.db.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.itis304.db.model.Income;
-import ru.itis304.db.model.IncomeData;
-import ru.itis304.db.model.IncomeType;
 import ru.itis304.db.repository.IncomeRepositoryBody;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TestLoad {
     public static void main(String[] args) throws ParseException, IOException {

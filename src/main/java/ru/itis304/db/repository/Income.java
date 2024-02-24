38ab25bc7d0f6ddf0b0f360c391cc0f4396336f6
Nbveh25 <tima.bikmukhametov@inbox.ru>
2024-02-24 11:17:24 +0300

    update income

---
+++ src/main/java/ru/itis304/db/repository/Income.java
@@ -0,0 +1,15 @@
+package ru.itis304.db.repository;
+
+public class Income implements IncomeRepository {
+
+    @Override
+    public ru.itis304.db.model.Income getAll() {
+        
+        return null;
+    }
+
+    @Override
+    public void save(ru.itis304.db.model.Income income) {
+
+    }
+}

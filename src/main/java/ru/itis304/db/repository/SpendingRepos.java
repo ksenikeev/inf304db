a01c19d1efc1839c28681510d9026d02938879d0
ToFik00 <tofikpetya@gmail.com>
2024-02-25 15:45:40 +0300

    refactor SpendingRepos.java

--- src/main/java/ru/itis304/db/repository/SpendingRepos.java
+++ src/main/java/ru/itis304/db/repository/SpendingRepos.java
@@ -3,47 +3,46 @@ package ru.itis304.db.repository;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import ru.itis304.db.model.Spending;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.itis304.db.model.SpendingData;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class SpendingRepos implements SpendingRepository {
 
     @Override
-    public Spending getAll() {
+    public List<SpendingData> findAll() {
         return null;
     }
 
     @Override
-    public void save(Spending income) {
+    public void save(SpendingData spendingData) {
+        List<SpendingData> dataList = findAll();
         JsonFactory jsonFactory = new JsonFactory();
-        JsonGenerator jGenerator = null;
+        JsonGenerator jGenerator;
         try {
             jGenerator = jsonFactory.createGenerator(new File("spending.json"), JsonEncoding.UTF8);
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            jGenerator.setCodec(new ObjectMapper());
 
             jGenerator.writeStartObject();
-
             jGenerator.writeFieldName("spending");
             jGenerator.writeStartArray();
 
-            for (int i = 0; i < income.getSpending().size(); i++) {
-                jGenerator.writeStartObject();
-                jGenerator.writeStringField("date", simpleDateFormat.format(income.getSpending().get(i).getDate()));
-                jGenerator.writeNumberField("sum", income.getSpending().get(i).getSum());
-                jGenerator.writeStringField("category", income.getSpending().get(i).getCategory().toString());
-                jGenerator.writeStringField("description", income.getSpending().get(i).getDescription());
-                jGenerator.writeEndObject();
-                //jGenerator.writeObject(income.getSpending().get(i));
-            }
-            jGenerator.writeEndArray();
+            for (SpendingData data : dataList) jGenerator.writeObject(data);
+            jGenerator.writeObject(spendingData);
 
+            jGenerator.writeEndArray();
             jGenerator.writeEndObject();
             jGenerator.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public List<SpendingData> findByPeriod(String dateFrom, String dateTo) {
+        return null;
     }
 }

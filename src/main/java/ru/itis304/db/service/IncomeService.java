16503e92a7a9a43ada82ad210f8b99071aa68373
DmitriyPetunin <154313405+DmitriyPetunin@users.noreply.github.com>
2024-03-01 19:48:11 +0300

    getAllByPeriod is ready

--- src/main/java/ru/itis304/db/service/IncomeService.java
+++ src/main/java/ru/itis304/db/service/IncomeService.java
@@ -7,6 +7,10 @@ import ru.itis304.db.model.SpendingData;
 import ru.itis304.db.repository.IncomeRepository;
 import ru.itis304.db.repository.SpendingRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class IncomeService implements IIncomeService {
@@ -36,8 +40,22 @@ public class IncomeService implements IIncomeService {
         return null;
     }
 
-    public List<IncomeData> getAllByPeriod(String dateFrom, String dateTo) {
-        return null;
+    public List<IncomeData> getAllByPeriod(String dateFrom, String dateTo){
+        List<IncomeData> list = new ArrayList<>();
+        Date datefrom = null;
+        Date dateto = null;
+        try {
+            datefrom = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateFrom);
+            dateto = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateTo);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        for (IncomeData incomeData : income.getIncome()) {
+            if (incomeData.getDate().after(datefrom) & incomeData.getDate().before(dateto)) {
+                list.add(incomeData);
+            }
+        }
+        return list;
     }
 
     public Float getSumByPeriod(String dateFrom, String dateTo) {

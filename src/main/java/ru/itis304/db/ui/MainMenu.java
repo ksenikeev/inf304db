40b8abf8e1f1cdafc93ed787227688d449bec502
DenchikBenz <vonbezin@yandex.ru>
2024-03-02 07:43:51 +0300

    Добавлена симуляция меню через цикл WhileTrue

--- src/main/java/ru/itis304/db/ui/MainMenu.java
+++ src/main/java/ru/itis304/db/ui/MainMenu.java
@@ -15,7 +15,7 @@ public class MainMenu {
     private ISpendingService spendingService;
 
     public MainMenu() {
-        scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         // иннициализация сервисов
         start();
     }
@@ -31,7 +31,42 @@ public class MainMenu {
          * 6. добавление новой позиции по расходу
          * 7. Подведение итогов за период (доходы - расходы), доходы, расходы
          * */
+        int p = scanner.nextInt();
+        System.out.println("1. Просмотр всех доходов\n" +
+                "2. Просмотр всех расходов\n" +
+                "3. Просмотр всех доходов за период\n" +
+                "4. Просмотр всех расходов за период\n" +
+                "5. Добавление новой позиции по доходу\n" +
+                "6. Добавление новой позиции по расходу\n" +
+                "7. Подведение итогов за период (доходы - расходы), доходы, расходы\n");
         while (true) {
+            switch (scanner.nextInt()){
+                case 1: showAllIncome();
+                    break;
+                case 2:
+                    shawAllSpending();
+                    break;
+                case 3:
+                    System.out.println("Введите даты начала и конца отсчета  в формате yyyy-MM-dd");
+                    showAllIncomePeriod(scanner.nextLine(),scanner.nextLine());
+                    break;
+                case 4:
+                    System.out.println("Введите даты начала и конца отсчета в формате yyyy-MM-dd");
+                    shawAllSpendingPeriod(scanner.nextLine(),scanner.nextLine());
+                    break;
+                case 5:
+                    addIncomeData();
+                    break;
+                case 6:
+                    addSpendingData();
+                    break;
+                case 7:
+                    showBalance();
+                    showIncomeSum();
+                    showSpendingSum();
+                    break;
+                default: System.out.println();
+            }
 
         }
     }
@@ -43,8 +78,16 @@ public class MainMenu {
 //        }
     }
     private void shawAllSpending() {}
-    private void showAllIncomePeriod() {}
-    private void shawAllSpendingPeriod() {}
+    private void showAllIncomePeriod(String dateFrom, String dateTo) {
+        for (int i = 0; i < incomeService.getAllByPeriod(dateFrom,dateTo).size();i ++){
+            System.out.println(String.valueOf(i+1) + incomeService.getAllByPeriod(dateFrom,dateTo).toArray()[i]);
+        }
+    }
+    private void shawAllSpendingPeriod(String dateFrom,String dateTo) {
+        for (int i = 0; i < spendingService.getAllByPeriod(dateFrom,dateTo).size();i ++){
+            System.out.println(String.valueOf(i+1) + spendingService.getAllByPeriod(dateFrom,dateTo).toArray()[i]);
+        }
+    }
     private void addIncomeData() {}
     private void addSpendingData() {}
     private void showBalance() {}
